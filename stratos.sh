3beb1b25302e47d7a45c9627dfdebe235581f486
SecorD0 <vovan6573@gmail.com>
2021-08-16 23:40:03 +0300

    Node auto-installation

--- stratos.sh
+++ stratos.sh
@@ -50,20 +50,20 @@ echo -e '\e[40m\e[92mWallet creating...\e[0m'
 $HOME/stratos/stchaincli keys add --hd-path "m/44'/606'/0'/0/0" --keyring-backend test --home $HOME/stratos/ $stratos_nodename &> "$HOME/stratos/$stratos_nodename.txt"
 stratos_address=$(grep -oP '(?<=\  address: )(\w+)' "$HOME/stratos/$stratos_nodename.txt")
 curl -s https://raw.githubusercontent.com/SecorD0/utils/main/insert_variable.sh | bash -s "stratos_address" $stratos_address
-curl -X POST https://faucet-test.thestratos.org/faucet/$Stratos_Address
+curl -X POST https://faucet-test.thestratos.org/faucet/$stratos_address
 echo -e '\e[40m\e[92mDone!\e[0m'
 curl -s https://raw.githubusercontent.com/SecorD0/utils/main/logo.sh | bash
 echo -e 'The guide of \e[40m\e[92mhttps://t.me/how_to_node\e[0m was used\n'
 echo -e '\nThe node was \e[40m\e[92mstarted\e[0m, the wallet was \e[40m\e[92mcreated\e[0m, the tokens was \e[40m\e[92mreceived\e[0m.\n'
 echo -e 'Remember to save this files:'
 echo -e "\e[40m\e[92m\"$HOME/stratos/$stratos_nodename.txt\"\e[0m"
-echo -e "\e[40m\e[92m\"$HOME/stratos/keyring-test-cosmos/$Stratos_Address.address\"\e[0m"
-echo -e "The wallet address: \e[40m\e[92m$Stratos_Address\e[0m"
-echo -e 'Enter "\e[40m\e[92msource ~/.bash_profile\e[0m" command or login to server again then you can use \e[40m\e[92m$stratos_nodename\e[0m and \e[40m\e[92m$Stratos_Address\e[0m variables (start to print variable then press "Tab")\n\n'
+echo -e "\e[40m\e[92m\"$HOME/stratos/keyring-test-cosmos/$stratos_address.address\"\e[0m"
+echo -e "The wallet address: \e[40m\e[92m$stratos_address\e[0m"
+echo -e 'You can use \e[40m\e[92m$stratos_nodename\e[0m and \e[40m\e[92m$stratos_address\e[0m variables (start to print variable then press "Tab")\n\n'
 echo -e '\tv \e[40m\e[92mUseful commands\e[0m v\n'
 echo -e 'To view the node status: \e[40m\e[92msystemctl status stratosd\e[0m'
 echo -e 'To view the node log: \e[40m\e[92mjournalctl -n 100 -f -u stratosd\e[0m'
 echo -e 'To view the node sync status: \e[40m\e[92m$HOME/stratos/stchaincli status 2>&1 | jq ."sync_info"."catching_up"\e[0m'
 echo -e 'To view latest block height: \e[40m\e[92m$HOME/stratos/stchaincli status 2>&1 | jq ."sync_info"."latest_block_height"\e[0m'
-echo -e 'To view the node balance (full sync required): \e[40m\e[92m$HOME/stratos/stchaincli query account $Stratos_Address --home $HOME/stratos/\e[0m'
+echo -e 'To view the node balance (full sync required): \e[40m\e[92m$HOME/stratos/stchaincli query account $stratos_address --home $HOME/stratos/\e[0m'
 echo -e 'To restart the node: \e[40m\e[92msystemctl restart stratosd\e[0m\n'
